421bb2f7f064bff8f9fcdea6166063b594d9f123
20Jayden <jhmoon0415@naver.com>
2021-10-31 01:51:54 +0900

    2021.10.30 - Google Login, OAuth2 생성

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -1,8 +1,18 @@
 var main = {
     init : function () {
         var _this = this;
+        /* id가 btn-save인 것을 click했을때 save 함수가 실행됩니다. */
         $('#btn-save').on('click', function(){
             _this.save();
+        });
+
+        /* id가 btn-update인 것을 click했을때 update 함수가 실행됩니다. */
+        $('#btn-update').on('click', function(){
+            _this.update();
+        });
+
+        $('#btn-delete').on('click', function(){
+            _this.delete();
         });
     },
     save : function (){
@@ -20,6 +30,42 @@ var main = {
             data: JSON.stringify(data)
         }).done(function(){
             alert('글이 등록되었습니다.');
+            window.location.href= '/';
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function (){
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function(){
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function (){
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function(){
+            alert('글이 삭제되었습니다.');
             window.location.href= '/';
         }).fail(function (error){
             alert(JSON.stringify(error));
